0f22d7afa2f56ea1d1904bb6794b78552bd2ecca
Felipe Manhani <fbmanhani@gmail.com>
2018-08-27 19:14:03 -0300

    Last class adjustments.

--- app/src/main/java/br/edu/ifsp/sdm/manhani/dicesdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/sdm/manhani/dicesdm/MainActivity.java
@@ -3,9 +3,11 @@ package br.edu.ifsp.sdm.manhani.dicesdm;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView resultadoImageView;
     private ImageView resultado2ImageView;
     private Spinner spinner;
+    private EditText numFacesEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,49 +32,63 @@ public class MainActivity extends AppCompatActivity {
         resultadoImageView = findViewById(R.id.resultadoImageView);
         resultado2ImageView = findViewById(R.id.resultado2ImageView);
         spinner = findViewById(R.id.spinner);
+        numFacesEditText = findViewById(R.id.numFacesEditText);
+        numFacesEditText.setHint("Campo Obrigatório...");
     }
 
 
     public void jogarDado(View view) {
         if (view.getId() == R.id.jogarDadoButton) {
             int numDices = Integer.parseInt(spinner.getSelectedItem().toString());
+            String numFacesStr = numFacesEditText.getText().toString().trim();
+            int numFaces = numFacesStr.equals("") ? 0 : Integer.parseInt(numFacesEditText.getText().toString());
 
-            if (numDices == 2) {
-                resultado2ImageView.setVisibility(View.VISIBLE);
-            } else {
-                resultado2ImageView.setVisibility(View.GONE);
-            }
 
-            for (int i = 1; i <= numDices; i++) {
-                int resultado = random.nextInt(6) + 1;
-
-                ImageView viewGenerica = i == 1 ? resultadoImageView : resultado2ImageView;
-
-                switch (resultado) {
-                    case 1:
-                        viewGenerica.setImageResource(R.drawable.dice_1);
-                        break;
-                    case 2:
-                        viewGenerica.setImageResource(R.drawable.dice_2);
-                        break;
-                    case 3:
-                        viewGenerica.setImageResource(R.drawable.dice_3);
-                        break;
-                    case 4:
-                        viewGenerica.setImageResource(R.drawable.dice_4);
-                        break;
-                    case 5:
-                        viewGenerica.setImageResource(R.drawable.dice_5);
-                        break;
-                    case 6:
-                        viewGenerica.setImageResource(R.drawable.dice_6);
-                        break;
-                    default:
-                        break;
+            if (numFaces > 0) {
+                if (numFaces > 6) {
+                    resultado2ImageView.setVisibility(View.GONE);
+                    resultadoImageView.setVisibility(View.GONE);
+                } else {
+                    resultadoImageView.setVisibility(View.VISIBLE);
+                    if (numDices == 2) {
+                        resultado2ImageView.setVisibility(View.VISIBLE);
+                    } else {
+                        resultado2ImageView.setVisibility(View.GONE);
+                    }
                 }
+
+                for (int i = 1; i <= numDices; i++) {
+                    int resultado = random.nextInt(numFaces) + 1;
+
+                    ImageView viewGenerica = i == 1 ? resultadoImageView : resultado2ImageView;
+
+                    switch (resultado) {
+                        case 1:
+                            viewGenerica.setImageResource(R.drawable.dice_1);
+                            break;
+                        case 2:
+                            viewGenerica.setImageResource(R.drawable.dice_2);
+                            break;
+                        case 3:
+                            viewGenerica.setImageResource(R.drawable.dice_3);
+                            break;
+                        case 4:
+                            viewGenerica.setImageResource(R.drawable.dice_4);
+                            break;
+                        case 5:
+                            viewGenerica.setImageResource(R.drawable.dice_5);
+                            break;
+                        case 6:
+                            viewGenerica.setImageResource(R.drawable.dice_6);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Número de faces precisa ser maior que zero", Toast.LENGTH_SHORT).show();
             }
 
-      //      resultadoTextView.setText(getString(R.string.face_sorteada) + resultado);
         }
     }
 }
